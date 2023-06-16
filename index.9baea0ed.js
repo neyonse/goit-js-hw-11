88b2ace8b4537269f83b6eb31dde8ac6d296388f
neyonse <121305819+neyonse@users.noreply.github.com>
2023-06-16 16:20:39 +0000

    Deploying to gh-pages from @ neyonse/goit-js-hw-11@9b62d38c9f14e408defde45068b136d0fb148c6b 🚀

---
+++ index.9baea0ed.js
@@ -0,0 +1,2 @@
+function e(e){const o=e.split(" ").join("+");console.log(o);const t={key:"37383891-385c0c3fa5b4b213da48ba87c",q:o,image_type:"photo",orientation:"horizontal",safesearch:!0};console.log(t)}const o=document.querySelector("#search-form"),t=o.querySelector('input[name="searchQuery"]');o.addEventListener("submit",(function(o){o.preventDefault();e(t.value)}));
+//# sourceMappingURL=index.9baea0ed.js.map
