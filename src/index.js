a0260d7eca838327564ae14f9368261443e6ceea
neyonse <neyonse@gmail.com>
2023-06-17 17:44:36 +0300

    homework in progress update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,103 @@
 import { fetchImages } from './js/fetch-api';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const formEl = document.querySelector('#search-form');
 const inputEl = formEl.querySelector('input[name="searchQuery"]');
+const galleryEl = document.querySelector('.gallery');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function refreshSimpleLightbox() {
+  lightbox.refresh();
+}
 
 formEl.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(e) {
   e.preventDefault();
   const inputData = getInputData();
-  fetchImages(inputData);
-  //   showSearchResults();
+
+  if (inputData === '') {
+    return;
+  }
+
+  if (galleryEl.hasChildNodes) {
+    galleryEl.innerHTML = '';
+  }
+
+  fetchImages(inputData)
+    .then(response => {
+      if (response.hits.length === 0) {
+        noSearchResults();
+        return;
+      }
+      showSearchResults(response);
+    })
+    .catch(error => {
+      console.error(error);
+    });
 }
 
 function getInputData() {
-  const inputData = inputEl.value;
-  return inputData;
+  return inputEl.value;
+}
+
+function noSearchResults() {
+  const message =
+    'Sorry, there are no images matching your search query. Please try again.';
+  Notify.failure(message, {
+    clickToClose: true,
+  });
+}
+
+function showSearchResults(response) {
+  response.hits.forEach(data => {
+    const dataToShow = getDataToShow(data);
+    createSearchResultsMarkup(dataToShow);
+  });
+  refreshSimpleLightbox();
+}
+
+function getDataToShow(data) {
+  const dataToShow = {
+    url: data.webformatURL,
+    largeImg: data.largeImageURL,
+    alt: data.tags,
+    likes: data.likes,
+    views: data.views,
+    comments: data.comments,
+    downloads: data.downloads,
+  };
+  console.log(dataToShow);
+  return dataToShow;
+}
+
+function createSearchResultsMarkup(dataToShow) {
+  const { url, largeImg, alt, likes, views, comments, downloads } = dataToShow;
+  const innerHTML = `
+  <div class="photo-card">
+    <a class="gallery__link" href="${largeImg}">
+    <img class="gallery__image src="${url}" alt="${alt}" loading="lazy" />
+    </a>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes ${likes}</b>
+      </p>
+      <p class="info-item">
+        <b>Views ${views}</b>
+      </p>
+      <p class="info-item">
+        <b>Comments ${comments}</b>
+      </p>
+      <p class="info-item">
+        <b>Downloads ${downloads}</b>
+      </p>
+    </div>
+  </div>`;
+  galleryEl.insertAdjacentHTML('beforeend', innerHTML);
 }
--- src/js/fetch-api.js
+++ src/js/fetch-api.js
@@ -1,11 +1,19 @@
+import axios from 'axios';
+
 const BASE_URL = 'https://pixabay.com/api/';
 const MY_API_KEY = '37383891-385c0c3fa5b4b213da48ba87c';
 
-export function fetchImages(inputData) {
-  const q = inputData.split(' ').join('+');
-  console.log(q);
+export async function fetchImages(inputData) {
+  const url = getFetchImagesUrl(inputData);
 
-  const options = {
+  const response = await axios.get(url);
+  return response.data;
+}
+
+function getFetchImagesUrl(inputData) {
+  const q = inputData.replace(' ', '+');
+
+  const params = {
     key: MY_API_KEY,
     q,
     image_type: 'photo',
@@ -13,18 +21,9 @@ export function fetchImages(inputData) {
     safesearch: true,
   };
 
-  console.log(options);
+  const paramString = Object.entries(params)
+    .map(([key, value]) => `${key}=${value}`)
+    .join('&');
 
-  //   const formattedOptions = JSON.stringify(options);
-  //   console.log(formattedOptions);
-
-  //   console.log(formattedOptions.split(':').join('=').split(',').join('&'));
-
-  //   const url = `${BASE_URL}?${formattedOptions}`;
-  //   console.log(url);
-
-  //   fetch(url)
-  //     .then(r => r.json())
-  //     .then(data => console.log(data));
-  //
+  return `${BASE_URL}?${paramString}`;
 }
